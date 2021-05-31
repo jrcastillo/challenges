43d5c0f013b92db39c60010fada331a2fa64a9ea
Ramon Castillo <ramon.castillo@payulatam.com>
2021-05-31 18:01:52 -0500

    adding readme and index js

---
+++ README.md
@@ -0,0 +1,3 @@
+# Yalo's Challenge
+
+## To run the test just do npm start
---
+++ index.js
@@ -0,0 +1,4 @@
+const sender = require('./sender');
+const testPath = `${process.cwd()}/sendTodir/testdata`;
+
+sender(testPath);
