7f6cfd62923986b0ac8080d69f9cf10edffdf375
Ramon Castillo <ramon.castillo@payulatam.com>
2021-05-31 18:00:47 -0500

    Adding autorun with index

--- sender.js
+++ sender.js
@@ -3,12 +3,11 @@ const Promise = require('bluebird');
 const requester = require('./requester');
 const readDirectory = require('./readDirectory');
 const csvParser = require('./csvParser');
-const testPath = `${process.cwd()}/sendTodir/testdata`;
 
 async function sendPayload(path) {
-  const filenames = readDirectory(path || testPath);
+  const filenames = readDirectory(path);
   return Promise.map(filenames, async (name) => {
-    const csv = await csvParser(`${testPath}/${name}`)
+    const csv = await csvParser(`${path}/${name}`)
     return requester.requestHandler(csv)
   }, {concurrency: 1})
 }
